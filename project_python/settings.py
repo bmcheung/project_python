8589da5b8b85d31e8cfe0463cd73c6845926da62
Nam Tran <namt214@gmail.com>
2016-10-26 16:11:56 -0700

    merge

--- project_python/settings.py
+++ project_python/settings.py
@@ -31,9 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.adoption',
     'apps.login',
-    'django_extensions',
+    # 'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
