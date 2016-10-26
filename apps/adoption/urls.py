b4933e309e8c01d0abe5ffaf4fedf375a1baecbd
Nam Tran <namt214@gmail.com>
2016-10-26 14:46:12 -0700

    bootstrap added

--- apps/adoption/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'^', include('apps.login.urls', namespace = "login")),
-    url(r'^home', include('apps.adoption.urls', namespace = "home")),
-]
