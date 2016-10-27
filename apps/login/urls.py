de6320d7d411c073984089dc3d8d36f0f8fc2923
Brian Cheung <bmcheung11@gmail.com>
2016-10-26 19:53:54 -0700

    added cats

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name = 'login'),
-    url(r'^process', views.process, name = 'process'),
-    url(r'^map', views.map, name = 'map'),
-    url(r'^adopt', views.adopt, name = 'adopt')
+    url(r'^$', views.index, name = 'login'),
+    url(r'^process/$', views.process, name = 'process'),
+    url(r'^map/$', views.map, name = 'map'),
+    url(r'^adopt/$', views.adopt, name = 'adopt')
 ]
